e3e4ee92b4f6c88544d151d4e0c573deb53dafbe
Conor Schaefer <conor@freedom.press>
2015-04-06 11:44:57 -0700

    adds url for TODO prebuild vagrant plugins

--- snap_ci/pinned.sh
+++ snap_ci/pinned.sh
@@ -7,6 +7,9 @@
 [[ -f ${SNAP_CACHE_DIR}/$vagrant_rpm ]] || wget https://dl.bintray.com/mitchellh/vagrant/$vagrant_rpm -O ${SNAP_CACHE_DIR}/$vagrant_rpm
 [[ -x /usr/bin/vagrant ]] || sudo -E rpm -ivh ${SNAP_CACHE_DIR}/$vagrant_rpm
 # TODO: Check for vagrant plugins before installing them.
+# Not sure if caching the vagrant plugins is going to save much time,
+# but this resource gives a good overview of the bundling procedure:
+# http://3ofcoins.net/2013/06/10/installing-vagrant-plugins-from-local-file/
 vagrant plugin install vagrant-digitalocean --plugin-version '0.7.0'
 vagrant plugin install vagrant-hostmanager
 [[ -f ${SNAP_CACHE_DIR}/digital_ocean.box ]] || wget https://github.com/smdahlen/vagrant-digitalocean/raw/master/box/digital_ocean.box -O ${SNAP_CACHE_DIR}/digital_ocean.box
