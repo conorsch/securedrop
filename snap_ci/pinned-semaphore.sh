c02c95ed3001749d2a650a7dd3c7c2f292b27431
Conor Schaefer <conor@freedom.press>
2015-11-26 10:44:30 -0800

    Semaphore setup: rubygems -> rubygems-integration

--- snap_ci/pinned-semaphore.sh
+++ snap_ci/pinned-semaphore.sh
@@ -33,7 +33,7 @@ vagrant plugin install vagrant-hostmanager
 
 # Install Ansible dependencies
 sudo apt install -y python-pip
-sudo apt install -y rubygems
+sudo apt install -y rubygems-integration
 # Install Ansible via pip for fine-grained version control
 sudo pip install ansible==1.9.0.1
 
