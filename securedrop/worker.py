a6aabb243ea68c3276dd462f783eb80623a53c84
Conor Schaefer <conor@freedom.press>
2017-11-12 18:57:34 -0500

    Makes Redis server host configurable
    In the development and app-prod deployment story, the redis server is
    always run on "localhost", so the value never needed to be changed. In
    the container story, however, "localhost" doesn't mean the same thing,
    so we'll need to provide customizations via env var to point to the
    "redis" server name.

--- securedrop/worker.py
+++ securedrop/worker.py
@@ -6,8 +6,10 @@ from rq import Queue
 queue_name = 'test' if os.environ.get(
     'SECUREDROP_ENV') == 'test' else 'default'
 
+REDIS_HOST = os.environ.get('SECUREDROP_REDIS_SERVER', 'localhost')
+
 # `srm` can take a long time on large files, so allow it run for up to an hour
-q = Queue(name=queue_name, connection=Redis(), default_timeout=3600)
+q = Queue(name=queue_name, connection=Redis(host=REDIS_HOST), default_timeout=3600)
 
 
 def enqueue(*args, **kwargs):
